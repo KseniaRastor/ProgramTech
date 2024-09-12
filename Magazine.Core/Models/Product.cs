7f0ad52f859148227eeccf6ed3e14e4b54b2c9b6
Ksenia <rastorguevak41@gmail.com>
2024-09-12 22:17:00 +0300

    edit class *Product*

---
+++ Magazine.Core/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magazine.Core.Models
+{
+    internal class Product
+    {
+        public int Id;
+        public string Definition;
+        public string Name;
+        public int Price;
+        public string Image;
+    }
+}
